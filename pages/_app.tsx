7b74d4b86677ecf5c521f545bf190516505c238d
Nishino0719 <nishino0719@icloud.com>
2020-12-04 20:03:10 +0900

    :zap:perf(tailwind): to fast compile tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import "tailwindcss/tailwind.css"
+import "../styles/tailwind.css"
 import '../styles/global.css'
+import "../styles/tailwind-util.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
