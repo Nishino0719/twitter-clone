825503151364fbd7645d87ff0a53919f6eeaffdc
Nishino0719 <nishino0719@icloud.com>
2020-12-17 07:40:21 +0900

    :hammer: feat(notificate): add notificate contents

---
+++ components/notificateheader.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const NotificateHeader = () => (
+  <div className="">
+    <div className="fixed w-full max-w-screen-sm bg-white z-10 border-r">
+      <div className="timeline-header flex relative mt-1">
+        <h3 className="font-semibold text-lg p-3 m-2">通知</h3>
+      </div>
+      <div className="flex w-full">
+        <div className="p-4 w-1/2 hover:bg-blue-50 hover:text-blue-400 font-semibold cursor-pointer text-gray-500 text-base border-b-2 border-blue-400 text-center">
+          <h3>すべて</h3>
+        </div>
+        <div className="p-4 w-1/2  hover:bg-blue-50 hover:text-blue-400 font-semibold cursor-pointer text-gray-500 text-base text-center">
+          <h3>@ツイート</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default NotificateHeader;
--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,13 +1,26 @@
+import Card from "../components/card";
+import NotificateHeader from "../components/notificateheader";
 import Recommended from "../components/recommended";
 import Sidebar from "../components/sidebar";
 import Timeline from "../components/timeline";
 import Trend from "../components/trend";
 
 export default function Notification() {
+  const array = [...Array(6)];
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center h-screen">
       <Sidebar></Sidebar>
-      <Timeline></Timeline>
+      <div className="max-w-screen-sm w-full border-l border-r overflow-scroll">
+        <NotificateHeader></NotificateHeader>
+        <div className="mt-32">
+          <div className="h-auto text-sm text-left block">
+            {/* TODO: トレンド一覧 */}
+            {array.map((_) => (
+              <Card></Card>
+            ))}
+          </div>
+        </div>
+      </div>
       <div>
         <Trend></Trend>
         <Recommended></Recommended>
